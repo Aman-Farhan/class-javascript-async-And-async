3cf63de83608ad323f52403083311ba8812fa6c3
Aman <amanfarhan2008@gmail.com>
2025-09-20 22:33:21 +0500

    second commit

--- app.js
+++ app.js
@@ -4,52 +4,7 @@
     
     
     
-// function signUp(email, pass, callBack, secondCallBack) {
-//     setTimeout(function () {
-//         if (email === "ta234567801@gmail.com" && pass === "12345678") {
-//             callBack(secondCallBack); 
-//         }
-//     }, 1000)
-// }
 
-
-
-// function signUp(email, pass,callBack,secondCallBack) {
-//     setTimeout(function(){
-//         if(email === "amanfarhan2008@gmail.com" && pass === "12345678"){
-//             callBack(secondCallBack)
-//         }
-//     }, 1000)
-// }
-
-
-// signUp("amanfarhan2008@gmail.com" , "12345678" ,
-//     function redirectToTimeLine(cb){
-//         console.log(cb()); 
-// } , function secondFunction(){
-//     return "Welcome Aman!"
-// });
-
-
-// signUp("amanfarhan2008@gmail.com", "12345678", 
-//     function redirectToTimeLine(cb) {
-//         console.log(cb());
-//     }, function secondFunction () {
-//         return "welcome Talal!"
-// });
-
-
-
-
-
-// signUp("amanfarhan2008@gmail.com" , "12345678");
-
-// signUp("ta234567801@gmail.com", "12345678", 
-//     function redirectToTimeLine(cb) {
-//         console.log(cb());
-//     }, function secondFunction () {
-//         return "welcome Talal!"
-// });
 
 
 
