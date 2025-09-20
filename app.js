7380e359cf95afccae3b76e9287406b5844ee5cf
Aman <amanfarhan2008@gmail.com>
2025-09-20 22:34:47 +0500

    3 commit

--- app.js
+++ app.js
@@ -3,14 +3,20 @@
     // }, 2000);
     
     
-    
 
 
+function signUp(email, pass,callBack,secondCallBack) {
+    setTimeout(function(){
+        if(email === "amanfarhan2008@gmail.com" && pass === "12345678"){
+            callBack(secondCallBack)
+        }
+    }, 1000)
+}
 
 
-
-
-
-
-
-
+signUp("amanfarhan2008@gmail.com" , "12345678" ,
+    function redirectToTimeLine(cb){
+        console.log(cb()); 
+} , function secondFunction(){
+    return "Welcome Aman!"
+});
